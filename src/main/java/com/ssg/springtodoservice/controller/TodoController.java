581eea8f2c266026fd116f8fa95fab49822aaba5
thatzcool <mycaptain0771@gmail.com>
2024-09-12 17:09:19 +0900

    spring-Day08_04

--- src/main/java/com/ssg/springtodoservice/controller/TodoController.java
+++ src/main/java/com/ssg/springtodoservice/controller/TodoController.java
@@ -1,26 +1,60 @@
 package com.ssg.springtodoservice.controller;
 
+import com.ssg.springtodoservice.dto.TodoDTO;
+import com.ssg.springtodoservice.service.TodoService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/todo")
 @Log4j2
+@RequiredArgsConstructor
 public class TodoController {
 
+    private final TodoService todoService;
+
+
     @RequestMapping("/list")
-    public void list(Model model) {
-        log.info("todo list...........");
+    public void list(){
+
+        log.info("todo list.......");
+
+
     }
-
-
     @GetMapping("/register")
     public void registerGET() {
-        log.info("GET  todo Register ......");
+        log.info("GET todo register.......");
     }
+
+    @PostMapping("/register")
+    public String registerPost(@Valid TodoDTO todoDTO,
+                               BindingResult bindingResult,
+                               RedirectAttributes redirectAttributes) {
+
+        log.info("POST todo register.......");
+
+        if(bindingResult.hasErrors()) {
+            log.info("has errors.......");
+            return "redirect:/todo/register";
+        }
+
+        log.info(todoDTO);
+
+        todoService.register(todoDTO);
+
+        return "redirect:/todo/list";
+    }
+
+
 
 
 }
--- src/main/java/com/ssg/springtodoservice/dto/TodoDTO.java
+++ src/main/java/com/ssg/springtodoservice/dto/TodoDTO.java
@@ -2,6 +2,8 @@ package com.ssg.springtodoservice.dto;
 
 import lombok.*;
 
+import javax.validation.constraints.Future;
+import javax.validation.constraints.NotEmpty;
 import java.time.LocalDate;
 
 @ToString
@@ -9,19 +11,24 @@ import java.time.LocalDate;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+
+//서버 사이드 데이터 검증 : 스프링 MVC에서는 파라미터 검증 작업은 컨트롤러에서 진행한다.
+// @Valid 와 BindingResult 객체를 이용하여 처리한다.
+// hibernate-validate 라이브러리가  필요하다
+
 public class TodoDTO {
 
     private Long tno;
 
-
+    @NotEmpty
     private String title;
 
-
+    @Future
     private LocalDate dueDate;
 
     private boolean finished;
 
-
+    @NotEmpty
     private String writer;
 
 }
